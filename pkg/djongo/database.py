0cf6b8972b015108ffd77c778de6265139ecef95
mostafa <mostafa.khaki@arvancloud.ir>
2024-12-22 14:51:18 +0330

    chore: remove default connect=False in mongo client creation

--- pkg/djongo/database.py
+++ pkg/djongo/database.py
@@ -9,12 +9,12 @@ def connect(db, **kwargs):
     if db not in clients:
         logger.debug('New MongoClient connection')
         # Create and reuse a single MongoClient instance
-        clients[db] = MongoClient(**kwargs, connect=False)
+        clients[db] = MongoClient(**kwargs)
     else:
         # Check if the existing MongoClient is closed
         if clients[db]._closed:  # Checking the internal attribute
             logger.debug('MongoClient was closed, creating a new connection')
-            clients[db] = MongoClient(**kwargs, connect=False)
+            clients[db] = MongoClient(**kwargs)
     return clients[db]
 
 
